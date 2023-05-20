96443adad778bd108fe95f390fb0b7d2740549f1
Bd 张 <bdzhang@192.168.2.104>
2023-05-20 13:17:26 +0800

    feat: reactive code

---
+++ reactive/reactive.js
@@ -0,0 +1,27 @@
+class Observer {
+    constructor (data) {
+        this.data = data
+        Object.keys(data).forEach((key) => {
+            if (typeof data[key] === 'object') {
+                new Observer(data[key])
+            } else {
+                this.defineReactive(this.data, key, this.data[key])
+            }
+        })
+    }
+
+    defineReactive (data, key ,val) {
+        Object.defineProperty(data, key, {
+            configurable: true,
+            enumerable: true,
+            get () {
+                console.log('getter', val)
+                return val
+            },
+            set (newValue) {
+                console.log('setter', newValue)
+                val = newValue
+            }
+        })
+    }
+}
