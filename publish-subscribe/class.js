2a2bfb9fc71f95348e83ff7e33617ccb7c58abd5
baoding_zhang <baoding_zhang@yunzhijia.com>
2023-05-23 18:00:39 +0800

    add code

--- publish-subscribe/class.js
+++ publish-subscribe/class.js
@@ -20,16 +20,38 @@ class Publish {
             fnList[i].apply(this, arguments)
         }
     }
+
+    remove (key, fn) {
+        let fnList = this.clientList[key]
+        if (!fnList) {
+            return false
+        }
+        if (!fn) {
+            fnList && (fnList.length = 0)
+        }
+        else {
+            for (let i = 0;i < fnList.length;i++) {
+                let _fn = fnList[i]
+                if (_fn === fn) {
+                    fnList.splice(i, 1)
+                }
+            }
+        }
+    }
 }
 
 let obj1 = new Publish()
 let obj2 = new Publish()
-obj1.listen('person1', (msg) => {
+obj1.listen('person1', fn1 = (msg) => {
     console.log('person1', msg)
+})
+obj1.listen('person1', fn2 = (msg) => {
+    console.log('再次person1', msg)
 })
 obj1.listen('person2', (msg) => {
     console.log('person2', msg)
 })
+obj1.remove('person1', fn1)
 obj1.trigger('person1', '触发person1')
 obj1.trigger('person2', '触发person2')
 
@@ -42,4 +64,3 @@ obj2.listen('person4', (msg) => {
 obj2.trigger('person3', '触发person3')
 obj2.trigger('person4', '触发person4')
 
-console.log(obj1)
